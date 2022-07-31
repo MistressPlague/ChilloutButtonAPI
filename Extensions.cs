955628f9a52527a7657be3ab6f420c92e36a907d
MistressPlague <krewellamusicps3@gmail.com>
2022-07-31 16:08:01 +0100

    publicize extensions

--- Extensions.cs
+++ Extensions.cs
@@ -11,7 +11,7 @@ using Object = UnityEngine.Object;
 
 namespace ChilloutButtonAPI
 {
-    internal static class Extensions
+    public static class Extensions
     {
         public static T GetOrAddComponent<T>(this GameObject obj) where T : Behaviour
         {
